a777afe9cf4f8769b71f7fad3e9bbc90ec8f1082
$a! <kalli.saikiran@outlook.com>
2020-05-25 11:49:16 -0400

    Adding HomeIndex.js to show restaurant food details in grid and Loginjs

---
+++ HomeIndex.js
@@ -0,0 +1,66 @@
+﻿$(function () {
+    
+    $(document).ready(function () {
+        $("#solTitle a").click(function () {
+            //Do stuff when clicked
+        });
+    });
+
+    LoadData();
+
+    $("#gridContainer").dxDataGrid({
+        dataSource: [],
+        showBorders: true,
+        selection: {
+            mode: "multiple"
+        },
+        filterRow: {
+            visible: true
+        },
+        paging: {
+            pageSize: 10
+        },
+        pager: {
+            showPageSizeSelector: true,
+            allowedPageSizes: [5, 10, 20],
+            showInfo: true
+        },
+        columns: [{ caption: 'ID', dataField: 'id', alignment: 'left', visible: false },
+            { caption: 'Item Quantity', dataField: 'Item_Quantity', alignment: 'left', visible: true },
+            { caption: 'Item Name', dataField: 'Item_Name', alignment: 'left', visible: true },
+            { caption: 'Item Customization', dataField: 'Item_Customization', alignment: 'left', visible: true },
+            { caption: 'Item Price', dataField: 'Item_Price', alignment: 'left', visible: true },
+        ]
+    });
+
+});
+function Logout() {
+    location.href = "../Restaurant/Login";
+}
+
+function SelectedRow() {
+    var array = $("#gridContainer").dxDataGrid("instance").getSelectedRowsData();
+    alert(array[0]);
+    $('#addlocationpopup').modal('show');
+
+    //return array[0];
+}
+
+
+function LoadData() {
+    $.ajax({
+        type: "POST",
+        url: "/Restaurant/GetData/",
+        dataType: "json",
+        data: '',
+        success: function (result) {
+            var Response = JSON.parse(result);
+
+            $("#gridContainer").dxDataGrid('instance').option('dataSource', Response.Table);
+        },
+        error: function (ex) {
+
+        }
+    });
+    return Response;
+}
---
+++ Login.js
@@ -0,0 +1,47 @@
+﻿$(function () {
+
+    
+    $(document).ready(function () {
+
+        $("#a1").click(function () {
+            var Username = document.getElementById("username").value;
+            var Password = document.getElementById("password").value;
+            if (Username == "" || Username == undefined) {
+                alert("Please enter or username");
+                return;
+            }
+            if (Password == "" || Password == undefined) {
+                alert("Please enter or Password");
+                return;
+            }
+            $.ajax({
+                url: "Restaurant/Login",
+                data: { 'username': Username, 'password': Password },
+                type: "POST",
+                success: function (data) {
+                    if (data == "Success") {
+                        //alert(data);
+                        location.href = "../Restaurant/HomeIndex";
+                    }
+                    else {
+                        location.href = "../Restaurant/Login";
+                    }
+                }
+            });
+
+        });
+
+    });
+
+});
+function Register() {
+    $.ajax({
+        url: "Restaurant/Register",
+        data: "",
+        type: "POST",
+        success: function (data) {
+            
+        }
+    });
+    
+}
