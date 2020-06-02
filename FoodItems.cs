70b329b0202a1603113a3edc93f6dcdbf747878b
Vinayyemula <64862508+Vinayyemula@users.noreply.github.com>
2020-06-02 09:53:03 -0400

    Add files via upload

---
+++ FoodItems.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_DataContract
+{
+    public class FoodItems
+    {
+        public int id { get; set; }
+
+        public int Item_Quantity { get; set; }
+
+        public string Item_Name { get; set; }
+
+        public string Item_Customization { get; set; }
+
+        public string Item_Price { get; set; }
+    }
+}
